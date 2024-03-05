757633dd1b31841592fc285f40608cd0831d81d7
Helton <heltonranieryoliveira@gmail.com>
2024-03-05 08:11:52 -0300

    Projeto final explorando programação reativa, ciclo do observable, operador RxJS, pipes do angular, Otimizando busca com RxJS e tratamento de erros

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,49 @@
+import { switchMap, map, tap, filter, debounceTime, throwError, catchError, EMPTY, of } from 'rxjs';
+import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { FormControl } from '@angular/forms';
+
+const PAUSA = 300;
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  campoBusca = new FormControl();
+  mensagemErro = ''
+  livrosResultado: LivrosResultado;
+
+  constructor(private service: LivroService) { }
+
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      tap(() => console.log('Fluxo inicial')),
+      switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+      map(resultado => this.livrosResultado = resultado),
+      tap((retornoAPI) => console.log(retornoAPI)),
+      map(resultado => resultado.items ?? []), // resultado.items ?? [] é a mesma coisa de: resultado.items ? resultado.items: []
+      map((items) => this.livrosResultadoParaLivros(items)),
+      catchError((erro) => {
+        // this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'
+        // return EMPTY
+        console.log(erro)
+        return throwError(() => new Error(this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'))
+      })
+    )
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
+
+}
+
+
